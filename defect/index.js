e26dbfc77c8ab5a29121ec4bdac65559127f5ffa
Jerry M <developer.jerrymannel@gmail.com>
2021-07-25 21:01:30 +0530

    Working menu navigation

--- defect/index.js
+++ defect/index.js
@@ -5,7 +5,9 @@ const menu = require("../menu")
 const display = require("../display")
 
 let defectDS = null;
+let defects = [];
 
+let count = parseInt(process.env.DS_COUNT) || 30
 let columnNames = process.env.DS_DEFECT_COL_NAMES.split(",")
 let columns = (process.env.DS_DEFECT_COL || "_id").split(",")
 let colWidth = process.env.DS_DEFECT_COL_WIDTH.split(",").map(_ => parseInt(_))
@@ -24,28 +26,41 @@ async function getDefectsBasedOnRelease(release) {
 	display(columnNames, columns, colWidth, defects)
 };
 
-async function getDefectsBasedOnStatus() {
-	let filter = {
-		"status": {
-			"$in": status
-		}
-	};
-	let defectCount = await defectDS.DataAPIs().CountRecords({
-		filter: filter
-	});
-	let defects = await defectDS.DataAPIs().ListRecords({
-		select: process.env.DS_DEFECT_COL || "_id",
-		filter: filter,
-		sort: "priority",
-		count: defectCount
-	});
-	display(columnNames, columns, colWidth, defects)
+async function getDefectsBasedOnStatus(status, page) {
+	while (true) {
+		if(page < 1) page = 1;
+		let filter = {
+			"status": {
+				"$in": status
+			}
+		};
+		console.log(JSON.stringify(filter))
+		let defectsFromAPI = await defectDS.DataAPIs().ListRecords({
+			select: process.env.DS_DEFECT_COL || "_id",
+			filter: filter,
+			sort: "priority",
+			count: count,
+			page: page
+		});
+		console.log()
+		if(defectsFromAPI.length == 0 ) {
+			page = page -1
+		} else defects = defectsFromAPI
+		display(columnNames, columns, colWidth, defects)
+		let option = await menu.select("Navigation > ", ["Next",  "Previous", "Exit"])
+		if(option == "Next") page += 1
+		if(option == "Previous") page -= 1
+		if(option == "Exit") break
+	}
 };
 
 async function entryPoint() {
-	let selection = await menu.select("Select:", ["vNext", "Open or Re-Open", "Other release"])
-	if(selection === "vNext") await getDefectsBasedOnRelease("vNext")
-	if(selection === "Open or Re-Open") await getDefectsBasedOnStatus(["Open", "Re-Open"])
+	while (true) {
+		let selection = await menu.select("Select:", ["vNext", "Open or Re-Open", "Other release", "Exit"])
+		if(selection === "vNext") await getDefectsBasedOnRelease("vNext")
+		if(selection === "Open or Re-Open") await getDefectsBasedOnStatus(["Open", "Re-Open"], 1)
+		if(selection == "Exit") break
+	}
 };
 
 module.exports = async () => {
--- display/index.js
+++ display/index.js
@@ -2,12 +2,13 @@ require("colors")
 var Table = require('cli-table3');
 
 module.exports = (col, keys, width, data) => {
+	console.clear()
 	let table = new Table({
 		head: col,
 		colWidths: width
 	});
-	data.forEach(d => {
-		let row = []
+	data.forEach((d, i) => {
+		let row = [i + 1]
 		keys.forEach(k => {
 			let value = k.split(".").reduce((p, c) => {
 				return p ? p[c] : 'Nil'.grey
